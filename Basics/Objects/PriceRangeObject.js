3729971820570dcf42fe33fa5355c71551177265
Harold Diaz <harold.diazv1@gmail.com>
2022-04-23 10:58:01 -0500

    Price range object to filter restaurant by cost

---
+++ Basics/Objects/PriceRangeObject.js
@@ -0,0 +1,12 @@
+// Price Range to list restaurants in a city
+
+let priceRange = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { label: '$$$', tooltip: 'Pricey', minPerPerson: 21, maxPerPerson: 40 }
+];
+
+let restaurants = [
+    { name: 'BrotherChicken', averagePerPerson: 5 },
+    { name: 'Chinese', averagePerPerson: 10 }
+]
