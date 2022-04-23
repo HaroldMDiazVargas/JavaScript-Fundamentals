39fe2690e93b3ec6db8260a5d0cc202f000887b5
Harold Diaz <harold.diazv1@gmail.com>
2022-04-23 15:49:40 -0500

    fix conditional lenght array

--- Basics/Arrays/getMax.js
+++ Basics/Arrays/getMax.js
@@ -3,10 +3,11 @@
 numbers = [1,2,3,4,5,6,1,2,3,9,-10,-20,20];
 const max = getMax(numbers);
 console.log(max);
-function getMax(array){
 
-    return array.reduce((accumulator,currentValue) => {
-        return (currentValue > accumulator)? accumulator=currentValue:accumulator;
-    },0);
+function getMax(array){
+    if (array.lenght === 0) return undefined;
+
+
+    return array.reduce((a,b) => (a > b)? a:b);
 
 }
