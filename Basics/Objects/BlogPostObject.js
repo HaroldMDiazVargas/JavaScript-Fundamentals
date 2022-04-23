9d79733342b02bee7a360e3bcffec6f26c8788d9
Harold Diaz <harold.diazv1@gmail.com>
2022-04-23 10:01:21 -0500

    Creating blog post object

---
+++ Basics/Objects/BlogPostObject.js
@@ -0,0 +1,14 @@
+const blogPost = {
+    title : 'JavaScript',
+    body : 'Abstract',
+    author : 'M. Diaz',
+    views : 15,
+    comments : [
+        { author: 'Smith', body: 'Content' },
+        { author: 'Sara', body: 'Content' },
+    ],
+    isLive : false
+};
+
+console.log(blogPost);
+
