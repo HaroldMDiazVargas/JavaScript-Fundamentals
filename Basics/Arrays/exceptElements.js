fa44e73635d95c102651fb09c41e7a81420bc770
Harold Diaz <harold.diazv1@gmail.com>
2022-04-23 13:03:31 -0500

    Fix file name

--- Basics/Arrays/exceptElements.js
+++ Basics/Arrays/exceptElements.js
@@ -6,7 +6,7 @@ console.log(output);
 
 
 function except(array, excluded){
-    const output = [];
+    const output = []
     for (let element of array)
         if (!excluded.includes(element))
             output.push(element)
